da8adbb57d44dc42c3d264fe8c35daaac4f3cee6
quiana2k4 <22686711.quynh@student.iuh.edu.vn>
2024-08-13 22:23:54 +0700

    FirstPrj.iml

---
+++ src/Welcome.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyễn Hồng Tuyết Quỳnh!");
+        System.out.println("Welcome to the DHKTPM19A!");
+    }
+}
